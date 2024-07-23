b448559bce357163f5b138c4d216ad48a85f9695
Alice <alicelsdev@gmail.com>
2024-07-23 11:08:13 -0300

    server working

--- data.py
+++ data.py
@@ -1,9 +1,12 @@
 import requests
 import vidsrc_search
-def popularMovies():
+from itertools import chain
+
+   
+def popularMovies(page):
     
 
-    url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
+    url = f"https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page={page}&sort_by=popularity.desc"
 
     headers = {
         "accept": "application/json",
@@ -15,15 +18,71 @@ def popularMovies():
     numberOfResults = len(response["results"])
 
     movies = [None]*numberOfResults
-    a = vidsrc_search()
-    print(a)
 
     for i in range (numberOfResults):
         image = f"http://image.tmdb.org/t/p/w500/{response["results"][i]["poster_path"]}"
         id = response["results"][i]["id"]
         link = f"https://vidsrc.to/embed/movie/{id}"
-        movies[i]=[response["results"][i]["title"], image, link]
+        movies[i]=[response["results"][i]["title"], image, link, id]
 
     return(movies)
 
-popularMovies()
+def popularSeries(page):
+    
+
+    url = f"https://api.themoviedb.org/3/discover/tv?include_adult=false&include_video=false&language=en-US&page={page}&sort_by=popularity.desc"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MjcxOWViYTE0YTJhNzhlODU1Y2ZkOWRiZWI4YWMxYiIsIm5iZiI6MTcyMDkwNTY3MS4zMjk3ODksInN1YiI6IjY2OTJlZWY5MWZlYjk4NDZmNzg5M2MxNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EZlh_1YVpTg-K-NUJKrDr3MoqTH4Cx9YgMW3KCIPkak"
+    }
+
+    response = requests.get(url, headers=headers).json()
+
+    numberOfResults = len(response["results"])
+
+    series = [None]*numberOfResults
+
+    for i in range (numberOfResults):
+        image = f"http://image.tmdb.org/t/p/w500/{response["results"][i]["poster_path"]}"
+        id = response["results"][i]["id"]
+        link = f"https://vidsrc.to/embed/tv/{id}"
+        series[i]=[response["results"][i]["name"], image, link, id]
+
+    return(series)
+
+def search(name):
+    url = f"https://api.themoviedb.org/3/search/multi?query={name}&include_adult=false&language=en-US&page=1"
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MjcxOWViYTE0YTJhNzhlODU1Y2ZkOWRiZWI4YWMxYiIsIm5iZiI6MTcyMDkwNTY3MS4zMjk3ODksInN1YiI6IjY2OTJlZWY5MWZlYjk4NDZmNzg5M2MxNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EZlh_1YVpTg-K-NUJKrDr3MoqTH4Cx9YgMW3KCIPkak"
+    }
+    response = requests.get(url, headers=headers).json()
+    numberOfResults = len(response["results"])
+
+    content = [None]*numberOfResults
+        
+    for i in range (numberOfResults):
+        id = response["results"][i]["id"]
+        
+        if response["results"][i]["media_type"]=="movie":
+            a = response["results"][i]["poster_path"]
+            image = f"http://image.tmdb.org/t/p/w500/{a}"
+            link = f"https://vidsrc.to/embed/movie/{id}"
+            if image != "http://image.tmdb.org/t/p/w500/None":
+                content[i]=[response["results"][i]["title"],image,link, id]
+        elif response["results"][i]["media_type"]=="tv":
+            a = response["results"][i]["poster_path"]
+            image = f"http://image.tmdb.org/t/p/w500/{a}"
+            link = f"https://vidsrc.to/embed/tv/{id}"
+            if image != "http://image.tmdb.org/t/p/w500/None":
+                content[i]=[response["results"][i]["name"],image,link, id]
+                            
+                
+    for j in range (len(content)):
+        if None in content:
+            content.remove(None)
+                
+            
+
+    return(content)
--- server.py
+++ server.py
@@ -1,29 +1,112 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import render_template
 from flask import request, send_file
 import data
+from data import popularMovies, popularSeries, search
+import json
+from flask_sqlalchemy import SQLAlchemy
+import random
+import ast
+from sqlalchemy.exc import IntegrityError
+
+
 
 
 app = Flask(__name__)
-
-
-@app.route("/")
-def home():
-    movies = data.popularMovies()
-    return render_template("index.html", movies = movies)
-    
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///favorites.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
     
 
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    title = db.Column(db.String(100), nullable=False)
+    image_url = db.Column(db.String(200), nullable=False)
+    link = db.Column(db.String(200), nullable=False)
+    
+def convert_string_to_list(string):
+    try:
+        # Use ast.literal_eval to safely parse the string
+        result = ast.literal_eval(string)
+        if isinstance(result, list):
+            return result
+        else:
+            raise ValueError("The string does not contain a list.")
+    except (ValueError, SyntaxError) as e:
+        print(f"Error converting string to list: {e}")
+        return None
 
-# @app.route('/result', methods=['GET', 'POST'])
-# def result():
-#     if request.method == 'POST' and len(dict(request.form)) > 0:
-#         data = dict(request.form)
-#         user = data["user"]
-#         getData.getData(user)
-#         return render_template("result.html")
-#     else:
-#         return "Sorry, there was an error."
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        page_number = int(request.form.get('page'))
+        new_movies = popularMovies(page_number)
+    else:
+        page_number = 1
+        # Initial movie list rendering
+        new_movies = popularMovies(page_number)
+
+    return render_template('index.html', movies=new_movies, page_number=page_number)
+
+@app.route('/series', methods=['GET', 'POST'])
+def series():
+    if request.method == 'POST':
+        page_number = int(request.form.get('page'))
+        new_movies = popularSeries(page_number)
+    else:
+        page_number = 1
+        # Initial movie list rendering
+        new_movies = popularSeries(page_number)
+
+    return render_template('index.html', movies=new_movies, page_number=page_number)
+
+@app.route('/search', methods=['GET', 'POST'])
+def searching():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        content = search(name)
+        return render_template('search.html', content=content)
+    
+    return render_template('search.html')
+
+@app.route('/favorite', methods=['GET', 'POST'])
+def loving():
+    try:
+        item = request.form.get('love')
+        res = convert_string_to_list(item)
+        print(res[3])
+        title = res[0]
+        image = res[1]
+        link = res [2]
+        id = res[3]
+        show = {"id": id, "title":title, "image_url":image, "link":link}
+        favorite = Favorite(id=show['id'], title=show['title'], image_url=show['image_url'], link=show['link'])
+        db.session.add(favorite)
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()  # Rollback the session to remove the failed insert operation
+        return render_template('search.html')
+    
+        
+    
+    return render_template('search.html')
+
+@app.route('/print_favorites')
+def print_favorites():
+    all_favorites = Favorite.query.all()
+    for favorite in all_favorites:
+        print(f"Title: {favorite.title}, Image URL: {favorite.image_url}, Link: {favorite.link}")
+    return "Favorites printed to console"
+
+@app.route('/favorites')
+def favorites():
+    all_favorites = Favorite.query.all()
+    return render_template('favorites.html', favorites=all_favorites)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Create the tables in the database
+    app.run(debug=True)
 
 
 if __name__ == "__main__":
